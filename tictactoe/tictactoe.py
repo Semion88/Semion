efa290af0770fab7ba25ce868d22619cef3607d4
Semion <s.m.kalashnykov@student.khai.edu>
2022-01-29 17:47:47 +0200

    2-nd commit

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -1,3 +1,26 @@
-print("X O X")
-print("O X O")
-print("X X O")
+vert_coord = ("a", "b","c")
+
+
+def get_igrok_char():
+    igrok_char = input("Select char (x,0): ").strip(" ").lower()
+    while igrok_char not in ("x","0"):
+        print("Oshibka")
+        igrok_char = input("Select char (x,0): ").strip(" ").lower()
+        return igrok_char
+
+
+pole = [["_" for x in range(3)] for y in range(3)]
+
+def show_pole(pole):
+    print(" ","1","2","3")
+    for y, v in enumerate(vert_coord):
+        print(v, pole[y])
+
+
+igrok_char = get_igrok_char()
+pc_char = "0" if igrok_char == "x" else "x"
+
+
+while True:
+    show_pole(pole)
+    input('igrat dalshe?')
