20152db0f196a6aeaae7c0a07f1d22ebde3440eb
Semion <s.m.kalashnykov@student.khai.edu>
2022-01-29 16:10:51 +0200

    2-nd commit

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,26 +1,29 @@
+import random
 print("Hangman")
 print("The game will be available soon.")
-
-
 def replace(guess_word):
-    a = []
+    a=[]
     for i in "guess_word":
         i = "-"
         a += i
         print("a")
         return a
-
-
-def guess(guess_word, a):
-    b = 8
+def guess(guess_word, a ):
+    k = 8
     input_letter = input("Input a letter: ")
     typed = set()
-    while b != 0:
+    while k != 0:
         if input_letter in guess_word:
             typed.add(input_letter)
             print([a if a in typed else "_" for a in guess_word])
             print(a)
         else:
             print("No such letter in the word")
-            b -= 1
+            k -= 1
             input_letter = input("Input a letter: ")
+print("H A N G  M  A N")
+a = []
+word_list = ["phython","java","kotlin","javascript"]
+guess_word = random.choice(word_list)
+print("guess_word")
+replace("guess_word")
