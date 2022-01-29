65571cb3996a77bedc19afd88c840f51198548a6
Semion <s.m.kalashnykov@student.khai.edu>
2022-01-29 17:07:16 +0200

    2-nd commit

--- coffemachine/coffemachine.py
+++ coffemachine/coffemachine.py
@@ -1,2 +1,18 @@
-print(
-    """1-Starting to make a coffee2-Grinding coffee beans3-Boiling water4-Mixing boiled water with crushed coffee beans5-Pouring coffee into the cup6-Pouring some milk into the cup7-Coffee is ready!""")
+print("How many cups you need ?:")
+cups = input()
+print('For ' + str(cups) + ' cups of coffee you will need:')
+water = cups * 200
+milk = cups * 50
+beans = cups * 15
+print(str(water) + ' ml of water')
+print(str(milk) + ' ml of milk')
+print(str(beans) + ' ml of coffee beans')
+print("""
+1-Starting to make a coffee
+2-Grinding coffee beans
+3-Boiling water
+4-Mixing boiled water with crushed coffee beans
+5-Pouring coffee into the cup
+6-Pouring some milk into the cup
+7-Coffee is ready!""")
+
