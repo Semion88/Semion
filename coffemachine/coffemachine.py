03839685a38def471641ce4542c8f7f6afaa8d70
Semion <s.m.kalashnykov@student.khai.edu>
2022-01-29 17:06:27 +0200

    1-st commit

---
+++ coffemachine/coffemachine.py
@@ -0,0 +1,2 @@
+print(
+    """1-Starting to make a coffee2-Grinding coffee beans3-Boiling water4-Mixing boiled water with crushed coffee beans5-Pouring coffee into the cup6-Pouring some milk into the cup7-Coffee is ready!""")
