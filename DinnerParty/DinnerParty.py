19ef79804037010c4afc2176121371b0365fec3e
Semion <s.m.kalashnykov@student.khai.edu>
2022-01-29 18:14:24 +0200

    4-th commit

--- DinnerParty/DinnerParty.py
+++ DinnerParty/DinnerParty.py
@@ -1,24 +1,33 @@
 import random
 
-friends = int(input("Enter the number of friends joining (including you):\n"))
-new_dict = {}
+friends = int(input("Enter the number of friends joining (including you):"))
 
 if friends > 0:
-    print("Enter the name of every friend (including you), each on a new line:\n")
+    print("Enter the name of every friend (including you), each on a new line:")
     name = [input() for i in range(friends)]
     new_dict = {name[x - 1]: 0 for x in range(friends + 1)}
-    Summa = int(input("Enter the total amount:"))
-    s = Summa / friends
-    z = round(s, 2)
-    new_dict = {name: z for name in new_dict}
-    print(new_dict)
+    sum = int(input("Enter the total amount: "))
+    new_dict_keys = list(new_dict.keys())
+    new_dict_keys.sort()
+
+
     while True:
-        g = input('Do you want to use the "Who is lucky?" feature? Write Yes/No:\n')
-        if g == 'Yes':
-
-
-        else:
-            print('No one is going to be lucky')
-            new_dict = {name: z for name in new_dict}
+        answer = input('Do you want to use the "Who is lucky?" feature? Write y/n:')
+        if answer == 'y':
+            list_amount = [round((sum / (friends - 1)), 2)] * friends
+            lucky_friend_index = random.choice(range(friends))
+            for i in range(len(new_dict_keys)):
+                new_dict[new_dict_keys[i]] = list_amount[i]
+            new_dict[new_dict_keys[lucky_friend_index]] = 0
+            print(f"{new_dict_keys[lucky_friend_index]} is the lucky one!")
             print(new_dict)
             break
+        elif answer == "n":
+            list_amount = [round((sum / friends), 2)] * friends
+            for i in range(len(new_dict_keys)):
+                new_dict[new_dict_keys[i]] = list_amount[i]
+            print("No one is going to be lucky.")
+            print(new_dict_keys)
+            break
+        else:
+            print("Incorrect choice. Try again!")
