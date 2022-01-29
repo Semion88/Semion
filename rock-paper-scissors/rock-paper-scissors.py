23bae854dec03045eba98c3fa8652888edc76fd4
Semion <s.m.kalashnykov@student.khai.edu>
2022-01-29 20:52:12 +0200

    3-rd commit

--- rock-paper-scissors/rock-paper-scissors.py
+++ rock-paper-scissors/rock-paper-scissors.py
@@ -1,14 +1,17 @@
 from random import choice
 
 print("Welcome To Rock-Paper-Scissors")
-player = input("Select any option\nrock\npaper\nscissors ").strip().lower()
-computer = choice("rps")
-if player == computer:
-    print("Draw")
-    print("Computer's choice -> ", computer)
-elif player == "rock" and computer == "scissors" or player == "paper" and computer == "rock" or player == "scissors" and computer == "paper":
-    print("Player Wins")
-    print("Sorry, but the computer chose another option and failed:", computer)
-else:
-    print("Enter another word")
-    print("Sorry, but the computer chose another option:", computer)
+while True:
+    player = input("Select any option\nrock\npaper\nscissors\nexit ").strip().lower()
+    computer = choice("rps")
+    if player == computer:
+        print("Draw")
+        print("Computer's choice -> ", computer)
+    elif player == "rock" and computer == "scissors" or player == "paper" and computer == "rock" or player == "scissors" and computer == "paper":
+        print("Player Wins")
+        print("Sorry, but the computer chose another option and failed:", computer)
+    else:
+        print("Enter another word")
+        print("Sorry, but the computer chose another option:", computer)
+    if player == "exit":
+     break
