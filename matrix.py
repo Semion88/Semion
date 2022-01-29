3dfc336a848bdbfd0b300e0205abee9feaedfb57
Semion <s.m.kalashnykov@student.khai.edu>
2022-01-29 21:41:30 +0200

    3-rd commit

--- matrix.py
+++ matrix.py
@@ -1,12 +1,12 @@
-def fill_matrix(r, c):
+def fill_matrix(rows, cols):
     try:
         matrix = []
         print("Введите значения матрицы:")
-        for row_idx in range(r):
+        for row_idx in range(rows):
             matrix.append([])
             user_input = input()
             matrix_values = user_input.split(" ")
-            for col_idx in range(c):
+            for col_idx in range(cols):
                 matrix[row_idx].append(int(matrix_values[col_idx]))
         return matrix
     except:
@@ -26,8 +26,8 @@ def matrix_sum(a_size, matrix_a, b_size, matrix_b):
         print("Error: matrices arent equal")
 
 
-def matrix_multiple(a_size, matrix_a):
-    const = int(input("Введите константу"))
+def matrix_multiple_on_const(a_size, matrix_a):
+    const = int(input("Введите константу: "))
     final_matrix = []
     for row_idx in range(int(a_size.split(' ')[0])):
         final_matrix.append([])
@@ -36,17 +36,52 @@ def matrix_multiple(a_size, matrix_a):
 
     return final_matrix
 
+
+def matrix_multiple(a_size, matrix_a, b_size, matrix_b):
+    if a_size.split(' ')[1] == b_size.split(' ')[0]:
+        final_matrix = []
+        for row_a_idx in range(int(a_size.split(' ')[0])):
+            final_matrix.append([])
+            for col_b_idx in range(int(b_size.split(' ')[1])):
+                multiplied_value = 0
+                for col_a_idx in range(int(a_size.split(' ')[1])): # 1
+                    multiplied_value += matrix_a[row_a_idx][col_a_idx] * matrix_b[col_a_idx][col_b_idx]
+                final_matrix[row_a_idx].append(multiplied_value)
+        return final_matrix
+    else:
+        print("Error: count of cols in first matrix must be equal to count of rows in second matrix")
+
+
 def main():
     try:
-        size_a = input("Введите размер матрицы A: ") # 4 4
+        size_a = input("Enter size of first matrix: ") # 4 4
         matrix_a = fill_matrix(int(size_a.split(" ")[0]), int(size_a.split(" ")[1]))
 
-        size_b = input("Введите размер матрицы B: ")
+        size_b = input("Enter size of second matrix: ")
         matrix_b = fill_matrix(int(size_b.split(" ")[0]), int(size_b.split(" ")[1]))
 
-        summed_matrix = matrix_sum(size_a, matrix_a, size_b, matrix_b)
-        print(summed_matrix)
-        multiplied_matrix = matrix_multiple(size_a, matrix_a)
-        print(multiplied_matrix)
+        while True:
+            print(
+                "Chose option \n"
+                "1. Add matrices \n"
+                "2. Multiply matrix by a constant \n"
+                "3. Multiply matrices \n"
+                "0. Exit"
+            )
+            user_input = int(input(""))
+
+            if user_input == 1:
+                summed_matrix = matrix_sum(size_a, matrix_a, size_b, matrix_b)
+                print(summed_matrix)
+            elif user_input == 2:
+                multiplied_on_const_matrix = matrix_multiple_on_const(size_a, matrix_a)
+                print(multiplied_on_const_matrix)
+            elif user_input == 3:
+                multiplied = matrix_multiple(size_a, matrix_a, size_b, matrix_b)
+                print(multiplied)
+            elif user_input == 0:
+                break
+            else:
+                print("Incorrect input try one's more")
     except:
         print("Ошибка: Неправельно введён размер матрицы")
