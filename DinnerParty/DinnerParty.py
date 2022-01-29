2fb6b4e77eca9a802891e0a9c5b81a7690259685
Semion <s.m.kalashnykov@student.khai.edu>
2022-01-29 18:12:07 +0200

    2-nd commit

--- DinnerParty/DinnerParty.py
+++ DinnerParty/DinnerParty.py
@@ -6,5 +6,9 @@ if friends > 0:
     name = [input() for i in range(friends)]
     new_dict = {name[x - 1]: 0 for x in range(friends + 1)}
     print(new_dict)
-else:
-    print("No one is joining for the party")
+    Summa = int(input("Enter the total amount:"))
+    if int(Summa / friends) != (Summa / friends):
+        New_Summa = [round((Summa / friends), 2)] * friends
+    else:
+        New_Summa = [round(Summa / friends)] * friends
+    new_dict_keys = list(new_dict.keys())
