c99795cbfe94584dd10e1370786f79cb4802d904
Semion <s.m.kalashnykov@student.khai.edu>
2022-01-29 21:40:55 +0200

    2-nd commit

--- matrix.py
+++ matrix.py
@@ -26,6 +26,16 @@ def matrix_sum(a_size, matrix_a, b_size, matrix_b):
         print("Error: matrices arent equal")
 
 
+def matrix_multiple(a_size, matrix_a):
+    const = int(input("Введите константу"))
+    final_matrix = []
+    for row_idx in range(int(a_size.split(' ')[0])):
+        final_matrix.append([])
+        for col_idx in range(int(a_size.split(' ')[1])):
+            final_matrix[row_idx].append(matrix_a[row_idx][col_idx] * const)
+
+    return final_matrix
+
 def main():
     try:
         size_a = input("Введите размер матрицы A: ") # 4 4
@@ -36,5 +46,7 @@ def main():
 
         summed_matrix = matrix_sum(size_a, matrix_a, size_b, matrix_b)
         print(summed_matrix)
+        multiplied_matrix = matrix_multiple(size_a, matrix_a)
+        print(multiplied_matrix)
     except:
         print("Ошибка: Неправельно введён размер матрицы")
