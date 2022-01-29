2e9979f925b3486ad80b3fe0f49ec6ece4f4a73b
Semion <s.m.kalashnykov@student.khai.edu>
2022-01-29 18:06:03 +0200

    3-rd commit

--- CreditCalculator/CreditCalculator.py
+++ CreditCalculator/CreditCalculator.py
@@ -1,4 +1,6 @@
 import math
+
+
 def CreditCalculator():
     print("What do you want to calculate?")
     print('type "m" for number of monthly payments,')
@@ -8,7 +10,7 @@ def CreditCalculator():
         principal = int(input("Enter the loan principal:"))
         payment = int(input("Enter the monthly payment:"))
         interest = int(input("Enter the loan interest:"))
-        x = payment / (payment - (interest/1200 * principal))
+        x = payment / (payment - (interest / 1200 * principal))
         degree = math.log(x, (interest / 1200 + 1))
         degree2 = math.ceil(degree)
         year = degree2 / 12
@@ -16,15 +18,21 @@ def CreditCalculator():
         r = str(round_up)
         y = list(r)
         print("It will take", y[0], "years and", y[2], "months to repay this loan!")
+    if x == 'q':
+        principal = int(input("Enter the loan principal:"))
+        periods = int(input("Enter the number of periods:"))
+        interest = int(input("Enter the loan interest:"))
+        a = (principal * (interest / 1200 * ((1 + interest / 1200) ** periods))) / (
+                    (1 + interest / 1200) ** periods - 1)
+        print("Your monthly payment =", math.ceil(a), "!")
     elif x == 'p':
         interest = float(input("Enter the loan interest:"))
-        b = interest / 1200 * (1 + interest / 1200) ** float(input("Enter the monthly payment:"))
-        c = (1 + interest / 1200) ** float(input("Enter the monthly payment:")) - 1
-        e = b / c
-        f = float(input("Enter the annuity payment:")) / e
-        g = round(f)
-        print("Your monthly payment =", g, "!")
+        f = float(input("Enter the annuity payment:")) / interest / 1200 * (1 + interest / 1200) ** float(
+            input("Enter the monthly payment:")) / (1 + interest / 1200) ** float(
+            input("Enter the monthly payment:")) - 1
+        print("Your monthly payment =", round(f), "!")
     else:
         print("Pleas try again")
+
 
 CreditCalculator()
