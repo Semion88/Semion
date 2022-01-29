d6374b954dd37243100e40e11a379b1265e95ca5
Semion <s.m.kalashnykov@student.khai.edu>
2022-01-29 20:54:32 +0200

    5-th commit

--- rock-paper-scissors/rock-paper-scissors.py
+++ rock-paper-scissors/rock-paper-scissors.py
@@ -18,6 +18,7 @@ values = {
     'snake': ['rock', 'fire', 'gun', 'lightning', 'devil', 'dragon'],
     'fire': ['fire', 'gun', 'lightning', 'devil', 'dragon', 'water'],
 }
+dashboard = {}
 
 
 def is_player_won(player_word, computer_word):
@@ -29,21 +30,53 @@ def is_player_won(player_word, computer_word):
 
 
 while True:
-    player_input = input(
-        "Select any option\nrock\npaper\nscissors\ngun\nlightning\ndevil\ndragon\nwater\nair\nsponge\nwolf\ntree\nhuman\nsnake\nfire\nexit\n ").strip()
-    computer_input = choice( ['rock', 'paper', 'scissors', 'gun', 'lightning', 'devil', 'dragon', 'water', 'air', 'sponge', 'wolf', 'tree',
-         'human', 'snake', 'fire'])
-    if player_input == computer_input:
-        print("Draw")
-        print("Computer's choice -> ", computer_input)
-    elif is_player_won(player_input, computer_input):
-        print("Player Wins")
-        print("Computer chose: ", computer_input)
-    elif not is_player_won(player_input, computer_input):
-        print("Computer Wins")
-        print("Computer chose: ", computer_input)
-    else:
-        print("Enter another word")
-        print("Sorry, but the computer chose another option:", computer_input)
-    if player_input == "exit":
-        break
+    try:
+        # New player adding
+        player_name = input("Enter player name: ")
+        if player_name not in dashboard:
+            dashboard[player_name] = 0
+        # Input player word
+        player_input = input(
+            "Select any option"
+            "- rock\n"
+            "- paper\n"
+            "- scissors\n"
+            "- gun\n"
+            "- lightning\n"
+            "- devil\n"
+            "- dragon\n"
+            "- water\n"
+            "- air\n"
+            "- sponge\n"
+            "- wolf\n"
+            "- tree\n"
+            "- human\n"
+            "- snake\n"
+            "- fire\n"
+            "- exit\n"
+        ).strip()
+        computer_input = choice(
+            ['rock', 'paper', 'scissors', 'gun', 'lightning', 'devil', 'dragon', 'water', 'air', 'sponge', 'wolf',
+             'tree',
+             'human', 'snake', 'fire'])
+        if player_input == computer_input:
+            dashboard[player_name] += 50
+            print("Draw")
+            print("Computer's choice: ", computer_input)
+            print(dashboard)
+        elif is_player_won(player_input, computer_input):
+            dashboard[player_name] += 100
+            print("Player Wins")
+            print("Computer chose: ", computer_input)
+            print(dashboard)
+        elif not is_player_won(player_input, computer_input):
+            print("Computer Wins")
+            print("Computer chose: ", computer_input)
+            print(dashboard)
+        else:
+            print("Enter another word")
+            print("Sorry, but the computer chose another option:", computer_input)
+        if player_input == "exit":
+            break
+    except:
+        print("Error: incorrect word. Try once more")
